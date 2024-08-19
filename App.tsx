eb885704502870f243914c162a10b8a6d7d98025
Ravi Kant <127819443+ravikant-216@users.noreply.github.com>
2024-08-19 19:50:06 +0530

    react scaffolding (#1)

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Hi Ravi</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "green",
+    fontWeight: 800,
+    fontSize: 24,
+  },
+});
